735b71b4233a907518318e2b0f578226118e5536
ErrorInever <errorinever@gmail.com>
2021-05-16 21:37:15 +0000

    add seed

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import torch
+import random
+import numpy as np
 from kornia.geometry.transform.crop.crop2d import center_crop
 
 
@@ -10,3 +13,16 @@ def center_crop_img(tensor, size, mode):
     :return:
     """
     return center_crop(tensor, size, mode)
+
+
+def get_random_noise():
+    pass
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+
