6b3816eb6444d6573af6d9d9a64744ebfc920693
ErrorInever <errorinever@gmail.com>
2021-05-19 00:27:08 +0000

    add fid transform

--- data/dataset.py
+++ data/dataset.py
@@ -7,10 +7,11 @@ from config import cfg
 
 class ImgFolderDataset(Dataset):
     """Custom dataset from folder where stored images"""
-    def __init__(self, img_folder_path):
+    def __init__(self, img_folder_path, fid=False):
         """
         :param img_folder_path: ``str``, path to folder where stored images
         """
+        self.fid = fid
         self.img_folder = img_folder_path
         self.img_names = [n for n in os.listdir(img_folder_path) if n.endswith(('png', 'jpeg', 'jpg'))]
 
@@ -19,7 +20,11 @@ class ImgFolderDataset(Dataset):
             img = Image.open(os.path.join(self.img_folder, self.img_names[idx])).convert('RGB')
         else:
             img = Image.open(os.path.join(self.img_folder, self.img_names[idx]))
-        return self.transform(img)
+
+        if self.fid:
+            return self.fid_transform(img)
+        else:
+            return self.transform(img)
 
     def __len__(self):
         return len(self.img_names)
@@ -30,9 +35,23 @@ class ImgFolderDataset(Dataset):
         Resizes images to resolution specified in config, does random horizontal flip with probability 0.5, convert to
         tensor and normalize
         """
-        return transforms.Compose([transforms.Resize(cfg.IMG_SIZE),
-                                   transforms.RandomHorizontalFlip(p=0.5),
-                                   transforms.ToTensor(),
-                                   transforms.Normalize(mean=[0.5 for _ in range(cfg.CHANNELS_IMG)],
-                                                        std=[0.5 for _ in range(cfg.CHANNELS_IMG)])
-                                   ])
+        return transforms.Compose([
+            transforms.Resize(cfg.IMG_SIZE),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5 for _ in range(cfg.CHANNELS_IMG)],
+                                 std=[0.5 for _ in range(cfg.CHANNELS_IMG)])
+            ])
+
+    @property
+    def fid_transform(self):
+        """
+        Default transform for inception_v3 fid
+        """
+        return transforms.Compose([
+            transforms.Resize(299),
+            transforms.CenterCrop(299),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ])
