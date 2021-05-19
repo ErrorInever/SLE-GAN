407cbc881304c72a30c8290ad4e4979f6a7ffc9b
ErrorInever <errorinever@gmail.com>
2021-05-19 00:34:50 +0000

    init inception_fid model

--- models/model.py
+++ models/model.py
@@ -1,7 +1,83 @@
 import torch
+import numpy as np
 import torch.nn as nn
+from tqdm import tqdm
+from scipy.linalg import sqrtm
 from kornia import filter2D
 from utils import center_crop_img
+from torchvision.models.inception import inception_v3
+
+
+class InceptionV3FID(nn.Module):
+    """Inception_v3 for calculate FID metric"""
+    def __init__(self, device):
+        super().__init__()
+        self.device = device
+        self.model = inception_v3(pretrained=True, progress=True, transform_input=True).to(self.device)
+        self.model.eval()
+        self.fc = self.model.fc
+        self.model.fc = nn.Sequential()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        """
+        :param x: ``Tensor([N, 3, 299, 299]), in range[-1, 1]``
+        """
+        # features map
+        x = self.model(x)
+        features = x.data.cpu().numpy()
+        # prediction
+        x = self.fc(x)
+        prob = self.softmax(x)
+        return features, prob
+
+    @staticmethod
+    def _calculate_stats(feature_map):
+        mu = np.mean(feature_map, axis=0)
+        sigma = np.cov(feature_map, rowvar=False)
+        return mu, sigma
+
+    @staticmethod
+    def _calculate_fid(mu1, sigma1, mu2, sigma2):
+        """
+        Calculate frechet inception distance
+        :param mu1: the feature-wise mean of the real data
+        :param sigma1: covariance matrix for the real data
+        :param mu2: the feature-wise mean of the generated data
+        :param sigma2: covariance matrix for the generated
+        :return:
+        """
+        ssdif = np.sum((mu1 - mu2)**2.0)
+        covmean = sqrtm(sigma1.dot(sigma2))
+        if np.iscomplexobj(covmean):
+            covmean = covmean.real
+        fid = ssdif + np.trace(sigma1 + sigma2 - 2.0 * covmean)
+        return fid
+
+    def _get_encoded_stats(self, dataloader, num_classes=1000):
+        """
+        Get encoded stats and probabilities from pool3_ft layer of inception_v3
+        :param dataloader: ``Instance of torch.utils.data.DataLoader``, dataloader
+        :param device: ``Instance of torch.device``, cuda device
+        :param num_classes: ``int``, number of classes of classifier, default = 1000
+        """
+        num_img = len(dataloader)
+        feature_map = np.zeros((num_img, 2048))
+        predictions = np.zeros((num_img, num_classes))
+
+        loop = tqdm(dataloader, leave=True)
+        for batch_idx, batch in enumerate(loop):
+            batch = batch.to(self.device)
+            batch_size = batch.shape[0]
+            i = batch_idx * batch_size
+            j = i + batch_size
+            feature_map[i:j], predictions[i:j] = self.forward(batch)
+
+        return feature_map
+
+    def get_fid_score(self):
+        # TODO: get fid score
+        pass
 
 
 class SLE(nn.Module):
