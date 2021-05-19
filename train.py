941f1718007e17aa4f3980f9fceeb4fd1d30ec34
ErrorInever <errorinever@gmail.com>
2021-05-19 23:42:28 +0000

    add evaluate

--- train.py
+++ train.py
@@ -4,16 +4,16 @@ import os
 import time
 import torch
 import torch.optim as optim
-
 from kornia.geometry.transform import resize
 from kornia.geometry.transform.crop.crop2d import center_crop
 
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
-from models.model import Generator, Discriminator
+from models.model import Generator, Discriminator, InceptionV3FID
 from config import cfg
-from utils import set_seed, save_checkpoint, load_checkpoint, get_random_noise, print_epoch_time, center_crop_img
-from data.dataset import ImgFolderDataset
+from utils import (set_seed, save_checkpoint, load_checkpoint, get_random_noise, print_epoch_time, center_crop_img,
+                   get_sample_dataloader)
+from data.dataset import ImgFolderDataset, FIDNoiseDataset
 from losses import hinge_adv_loss, reconstruction_loss
 
 
@@ -24,16 +24,17 @@ def parse_args():
     parser.add_argument('--checkpoint', dest='checkpoint', help='path to checkpoint.pth.tar', default=None, type=str)
     parser.add_argument('--device', dest='device', help='use device: gpu, tpu. Default use gpu if available',
                         default='gpu', type=str)
+    parser.add_argument('--fid', dest='fid', help='display fid score', action='store_true', type=bool)
     parser.add_argument('--wandb_id', dest='wandb_id', help='wand metric id for resume', default=None, type=str)
-    parser.add_argument('--wandb_key', dest='wandb_key', help='use this option if you run it from kaggle',
-                        default=None, type=str)
+    parser.add_argument('--wandb_key', dest='wandb_key', help='use this option if you run it from kaggle, '
+                                                              'input api key', default=None, type=str)
     parser.print_help()
     return parser.parse_args()
 
 
 @print_epoch_time
 def train_one_epoch(gen, opt_gen, scaler_gen, dis, opt_dis, scaler_dis, dataloader, metric_logger, device,
-                    fixed_noise, epoch):
+                    fixed_noise, epoch, fid_model, fid_score):
     """
     Train one epoch
     :param gen: ``Instance of models.model.Generator``, generator model
@@ -85,12 +86,32 @@ def train_one_epoch(gen, opt_gen, scaler_gen, dis, opt_dis, scaler_dis, dataload
         scaler_gen.step(opt_gen)
         scaler_gen.update()
 
-        # TODO: metrics
+        if fid_score:
+            if epoch % cfg.FID_FREQ == 0:
+                fid = evaluate(gen, fid_model)
+                # TODO: display fid metric_logger
+
+        # Metrics
+        if batch_idx % cfg.FREQ == 0:
+            # TODO: display status
+            pass
 
 
-def evaluate(gen, inception_fid, real_dataloader, fake_dataloader, ):
-    # TODO dataloaders
-    pass
+def evaluate(gen, fid_model):
+    real_dataset = ImgFolderDataset(cfg.DATASET_PATH, fid=True)
+    real_dataloader = get_sample_dataloader(real_dataset, num_samples=cfg.FID_NUM_SAMPLES,
+                                            batch_size=cfg.BATCH_SIZE)
+    noise = torch.randn([len(real_dataloader), cfg.Z_DIMENSION, 1, 1])
+    fake_images = []
+    for batch in noise:
+        fake_images.append(gen(batch.unsqueeze(0)))
+
+    noise_dataset = FIDNoiseDataset(fake_images)
+    fake_dataloader = DataLoader(noise_dataset, batch_size=cfg.BATCH_SIZE)
+
+    fid = fid_model.get_fid_score(real_dataloader, fake_dataloader)
+
+    return fid
 
 
 if __name__ == '__main__':
@@ -100,6 +121,8 @@ if __name__ == '__main__':
     args = parse_args()
 
     assert args.data_path, 'data path not specified'
+
+    cfg.DATASET_PATH = args.data_path
 
     if args.wandb_key:
         os.environ["WANDB_API_KEY"] = args.wandb_key
@@ -122,6 +145,11 @@ if __name__ == '__main__':
         logger.error(f"device:{args.device}", exc_info=True)
         raise ValueError('Device not specified and gpu is not available')
     logger.info(f'Using device:{args.device}')
+
+    if args.fid:
+        fid_model = InceptionV3FID(device)
+    else:
+        fid_model = None
 
     # defining dataset and dataloader
     dataset = ImgFolderDataset(args.data_path)
