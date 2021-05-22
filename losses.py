fix gradient error

losses.py
@@ -20,6 +20,6 @@ def hinge_adv_loss(real_fake_logits_real, real_fake_logits_fake):
     :param real_fake_logits_fake: ``Tensor([1, 5, 5])``
     :return: ``float``, discriminator loss
     """
-    real_loss = -1 * torch.mean(torch.minimum(torch.Tensor([0.0]), -1 + real_fake_logits_real.cpu()))
-    fake_loss = -1 * torch.mean(torch.minimum(torch.Tensor([0.0]), -1 - real_fake_logits_fake.cpu()))
+    real_loss = -1 * torch.mean(torch.minimum(torch.Tensor([0.0]), -1 + real_fake_logits_real.detach().cpu()))
+    fake_loss = -1 * torch.mean(torch.minimum(torch.Tensor([0.0]), -1 - real_fake_logits_fake.detach().cpu()))
     return real_loss + fake_loss
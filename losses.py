0195840df104f5d6965c3f769df775df20284918
ErrorInever <errorinever@gmail.com>
2021-05-22 02:44:13 +0000

    fix gradient error

--- losses.py
+++ losses.py
@@ -20,6 +20,6 @@ def hinge_adv_loss(real_fake_logits_real, real_fake_logits_fake):
     :param real_fake_logits_fake: ``Tensor([1, 5, 5])``
     :return: ``float``, discriminator loss
     """
-    real_loss = -1 * torch.mean(torch.minimum(torch.Tensor([0.0]), -1 + real_fake_logits_real.cpu()))
-    fake_loss = -1 * torch.mean(torch.minimum(torch.Tensor([0.0]), -1 - real_fake_logits_fake.cpu()))
+    real_loss = -1 * torch.mean(torch.minimum(torch.Tensor([0.0]), -1 + real_fake_logits_real.detach().cpu()))
+    fake_loss = -1 * torch.mean(torch.minimum(torch.Tensor([0.0]), -1 - real_fake_logits_fake.detach().cpu()))
     return real_loss + fake_loss
