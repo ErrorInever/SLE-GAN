fix gradient NaN

train.py
@@ -62,7 +62,7 @@ def train_one_epoch(gen, opt_gen, scaler_gen, dis, opt_dis, scaler_dis, dataload
             # Hinge adversarial loss: -E[min(0, -1 + D(x)] - E[min(0, -1 + D(x_hat)] + reconstruction loss
             fake = gen(noise)
             real_fake_logits_real, decoded_real_img_cropped, decoded_real_img = dis(real)
-            real_fake_logits_fake, _, _ = dis(fake)
+            real_fake_logits_fake, _, _ = dis(fake.deatch())
             logits_loss = hinge_adv_loss(real_fake_logits_real, real_fake_logits_fake)
             i_recons_loss = reconstruction_loss(real_128, decoded_real_img)
             i_part_recons_loss = reconstruction_loss(real_cropped_128, decoded_real_img_cropped)
@@ -76,7 +76,8 @@ def train_one_epoch(gen, opt_gen, scaler_gen, dis, opt_dis, scaler_dis, dataload
         # Train generator
         with torch.cuda.amp.autocast():
             # We maximize E[D(G(z))] or minimize the negative of that
-            g_loss = -1 * real_fake_logits_fake.mean()
+            gen_real_fake_logits_fake, _, _ = dis(fake)
+            g_loss = -torch.mean(gen_real_fake_logits_fake)
 
         opt_gen.zero_grad()
         scaler_gen.scale(g_loss).backward()